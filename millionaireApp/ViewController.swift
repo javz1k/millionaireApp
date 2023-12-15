739d9313525ede0ebff72f0ce06dc52098f6c449
javz1k <java93@live.com>
2023-12-15 16:28:02 +0400

    Initial Commit

---
+++ millionaireApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  millionaireApp
+//
+//  Created by Cavidan Mustafayev on 15.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
